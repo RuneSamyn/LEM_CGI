b0ca4fe1c915f0c91eccc4e9c345e38d0ef88e1c
Rune Samyn <rune.samyn@gmail.com>
2020-11-23 09:10:21 +0000

    get post request

--- sources/main.c
+++ sources/main.c
@@ -1,65 +1,124 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAXLEN 80
-#define EXTRA 5
-/* 4 for field name "data", 1 for "=" */
-#define MAXINPUT MAXLEN+EXTRA+2
-/* 1 for added line break, 1 for trailing NUL */
-#define DATAFILE "data.txt"
+#define MAXLEN 600
+#define MAXINPUT 600
 
-
-void 
-unencode(char *src, char *last, char *dest)
+struct Persoon
 {
- for(; src != last; src++, dest++)
-   if(*src == '+')
-     *dest = ' ';
-   else if(*src == '%') {
-     int code;
-     if(sscanf(src+1, "%2x", &code) != 1) code = '?';
-     *dest = code;
-     src +=2; }     
-   else
-     *dest = *src;
- *dest = '\n';
- *++dest = '\0';
+  char Voornaam[25];
+  char Naam[25];
+  char Job[25];
+  char Tel[15];
+  long TimeStamp;
+};
+
+static void print_http_header(const char *content_type)
+{
+  printf("Content-Type: %s\n\n", content_type);
 }
 
-int
-printFile(FILE *f) {
+void unencode(char *src, char *last, char *dest)
+{
+  for (; src != last; src++, dest++)
+    if (*src == '+')
+      *dest = ' ';
+    else if (*src == '%')
+    {
+      int code;
+      if (sscanf(src + 1, "%2x", &code) != 1)
+        code = '?';
+      *dest = code;
+      src += 2;
+    }
+    else
+      *dest = *src;
+  *dest = '\n';
+  *++dest = '\0';
+}
+
+int printFile(FILE *f)
+{
   int c;
-  if(f != NULL) {
+  if (f != NULL)
+  {
     while ((c = getc(f)) != EOF)
       printf("%c", c);
     fclose(f);
   }
 }
 
-int 
-main ()
+char *findInString(char in[600], char name[10])
 {
-    printf("Content-type: text/html\n\n");
-    FILE *html = fopen("/usr/lib/cgi-bin/index.html", "r");
-    printFile(html);
-    // char *lenstr;
-    // char input[MAXINPUT], data[MAXINPUT];
-    // long len;
-    // lenstr = getenv("CONTENT_LENGTH");
-    // if(lenstr == NULL || sscanf(lenstr,"%ld",&len)!=1 || len > MAXLEN)
-    //     printf("<P>No post request.</p>");
-    // else {
-    //     FILE *f;
-    //     fgets(input, len+1, stdin);
-    //     unencode(input+EXTRA, input+len, data);
-    //     f = fopen(DATAFILE, "a");
-    //     if(f == NULL)
-    //         printf("<P>Sorry, cannot store your data.");
-    //     else
-    //         fputs(data, f);
-    //     fclose(f);
-    //     printf("<P>Thank you! Your contribution has been stored.");
-    // }
+  char s[30];
+  snprintf(s, 30,"name=\"%s\"", name); // name="voornaam" => search after this kind of string
+  char e[10] = "\r\n------";
+  // Extract the first substring
+  char *subString; 
+  subString = strtok(in, e);
+  // loop through the string to extract all substrings
+  int nextIsAnser = 0;
+  while( subString != NULL ) {
+    if(nextIsAnser == 1) {
+      return (char*)subString;
+      nextIsAnser = 0;
+    }
+    if(strstr(subString, s) != NULL) {
+      nextIsAnser = 1;
+    }
+    subString = strtok(NULL, e);
+  }
+  return NULL;
+}
+
+int post_handler()
+{
+  char *lenstr;
+  long len;
+  lenstr = getenv("CONTENT_LENGTH");
+  if (lenstr == NULL || sscanf(lenstr, "%ld", &len) != 1 || MAXLEN > 1000)
+  {
+    printf("<P>No post request.</p>");
     return 0;
+  }
+  char input[len];
+  int i = 0;
+  while (i <= len)    // read the post data
+    input[i++] = getc(stdin);
+  struct Persoon p;
+  // strcpy(p.Voornaam, "Rune");
+  // printf(p.Voornaam);
+  char *cpy = strdup(input);
+  strcpy(p.Voornaam, findInString(cpy, (char*)"voornaam"));
+  cpy = strdup(input);
+  strcpy(p.Naam, findInString(cpy, (char*)"naam"));
+  cpy = strdup(input);
+  strcpy(p.Job, findInString(cpy, (char*)"job"));
+  cpy = strdup(input);
+  strcpy(p.Tel, findInString(cpy, (char*)"tel"));
+  p.TimeStamp = (int)time(NULL);
+  printf("<P>Thank you! Your contribution has been stored.");
+  return 0;
+}
+
+int main()
+{
+  print_http_header("text/html");
+  FILE *html = fopen("/usr/lib/cgi-bin/index.html", "r");
+  printFile(html);
+  post_handler();
+  // char input[600] = "hallo name=\"voornaam\"\r\n\r\nRune\r\n-----------";
+  // char naam[10] = "voornaam";
+  // findInString(input, naam);
+  
+  FILE *f = fopen("/var/www/html/test.json", "a");
+  if(f == NULL)
+      printf("<P>Sorry, cannot store your data.");
+  else
+    fputs("hello world", f);
+    printf("hello world");
+  fclose(f);
+  return 0;
 }
